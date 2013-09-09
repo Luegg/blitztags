8c018a9402bcb90f0846658420efaf7bc63c97d9
Luegg <wegmaluk@gmail.com>
2013-09-09 16:48:31 +0200

    introduced print methods in templates

--- src/main/scala/blitztags/Template.scala
+++ src/main/scala/blitztags/Template.scala
@@ -4,4 +4,8 @@ trait Template {
   implicit val builder = new XmlBuilder{}
   
   def document = builder.document.docElem
+  
+  def prettyPrint: String
+  
+  def miniPrint: String
 }
--- src/main/scala/blitztags/html5/Template.scala
+++ src/main/scala/blitztags/html5/Template.scala
@@ -1,5 +1,19 @@
 package blitztags.html5
 
+import scala.xml.PrettyPrinter
+
 trait Template extends blitztags.Template{
-  val Tags = blitztags.html5.Tags
+  val tags = blitztags.html5.Tags
+  
+  val doctype = "<!DOCTYPE html>"
+    
+  val prettyPrinter = new PrettyPrinter(80, 4)
+    
+  def prettyPrint = {
+    doctype + "/n" + prettyPrinter.format(document)
+  }
+    
+  def miniPrint = {
+    doctype + "/n" + document.toString
+  }
 }
