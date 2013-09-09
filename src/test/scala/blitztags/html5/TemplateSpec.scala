b42d80663f3990f3e501e6e4d371708514b27a47
Luegg <wegmaluk@gmail.com>
2013-09-09 17:02:59 +0200

    template specs

---
+++ src/test/scala/blitztags/html5/TemplateSpec.scala
@@ -0,0 +1,24 @@
+package blitztags.html5
+
+import org.scalatest.FlatSpec
+import org.scalatest.matchers.ShouldMatchers
+import Tags._
+
+class TemplateSpec extends FlatSpec with ShouldMatchers {
+  "a HTML5 template" should "print html" in {
+    case class Page(title: String) extends Template {
+      Html {
+        Head {
+          Title { title }
+        }
+        Body {
+          H1 { title }
+          P { "Hello blitztags!" }
+        }
+      }
+    }
+    
+    Page("Welcome").miniPrint should be ("<!DOCTYPE html>\n"
+        + "<html><head><title>Welcome</title></head><body><h1>Welcome</h1><p>Hello blitztags!</p></body></html>")
+  }
+}
