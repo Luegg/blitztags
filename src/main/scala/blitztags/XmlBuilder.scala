3535ab5aa4bebf29632bddde17e618c50126234d
Luegg <wegmaluk@gmail.com>
2013-09-10 14:33:37 +0200

    some privacy for mutable state

--- src/main/scala/blitztags/XmlBuilder.scala
+++ src/main/scala/blitztags/XmlBuilder.scala
@@ -3,27 +3,27 @@ package blitztags
 import scala.xml._
 
 trait XmlBuilder {
-  trait XmlWrapper {
+  private[this] trait XmlWrapper {
     def addChild(n: Node): XmlWrapper
   }
 
-  implicit class ExtDocument(val doc: Document) extends XmlWrapper {
+  implicit private[this] class ExtDocument(val doc: Document) extends XmlWrapper {
     def addChild(n: Node) = {
       doc.docElem = n
       doc
     }
   }
 
-  implicit class ExtElem(val elem: Elem) extends XmlWrapper {
+  implicit private[this] class ExtElem(val elem: Elem) extends XmlWrapper {
     def addChild(n: Node) = elem.copy(child = elem.child.toSeq ++ n)
   }
 
   val document = new Document()
 
   // explicitly apply implicit class because of issues with scalamock
-  var currentNode: XmlWrapper = new ExtDocument(document)
+  private var currentNode: XmlWrapper = new ExtDocument(document)
 
-  var workStack: List[XmlWrapper] = Nil
+  private var workStack: List[XmlWrapper] = Nil
 
   def addChild(n: Node): Unit = {
     currentNode = currentNode.addChild(n)
